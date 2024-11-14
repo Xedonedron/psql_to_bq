4f765139156ca12ed614a59b78f0b3f5fe350ec1
Nixon Daniel Hutahaean <nixondanielhutahaean@gmail.com>
2024-11-14 14:57:32 +0700

    Update dummy.py

--- dags/dummy.py
+++ dags/dummy.py
@@ -1,7 +1,6 @@
 import pendulum
 
 from airflow import models
-from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 import pytz
@@ -16,12 +15,8 @@ with models.DAG(
     tags=['templated', 'TESTING'],
 ) as dag:
     
-    dummy_task = DummyOperator(
-        task_id="Start task",
-    )
-
     bash_task = BashOperator(
     task_id="bash_task",
     bash_command='echo "Hi from bash operator"')
 
-    dummy_task >> bash_task
+    bash_task
